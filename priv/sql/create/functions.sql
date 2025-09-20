545acd14c442483b489a6e261e1c56f3328c626b
Kacaii <kacaii.privado@gmail.com>
2025-09-20 16:14:36 -0300

    :lipstick: use parameter names instead of positional arguments on sql

--- priv/sql/create/functions.sql
+++ priv/sql/create/functions.sql
@@ -7,7 +7,7 @@ DROP FUNCTION IF EXISTS public.get_category_id_by_name;
 --   CREATE -----------------------------------------------------------------
 
 --   In case we need the database ID
-CREATE OR REPLACE FUNCTION public.get_user_id_by_registration(registration TEXT)
+CREATE OR REPLACE FUNCTION public.get_user_id_by_registration(rg TEXT)
 RETURNS UUID AS $$
 
 DECLARE user_id UUID;
@@ -16,7 +16,7 @@ BEGIN
 
 SELECT u.id INTO user_id
   FROM public.user_account AS u
-WHERE u.registration = $1;
+WHERE u.registration = rg;
 
 RETURN user_id;
 
@@ -24,7 +24,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 --   In case we only know the name.
-CREATE OR REPLACE FUNCTION public.get_category_id_by_name(name TEXT)
+CREATE OR REPLACE FUNCTION public.get_category_id_by_name(input_name TEXT)
 RETURNS UUID AS $$
 
 DECLARE category_id UUID;
@@ -33,7 +33,7 @@ BEGIN
 
 SELECT oc.id INTO category_id
 FROM public.occurrence_category AS oc
-WHERE oc.category_name = $1;
+WHERE oc.category_name = input_name;
 
 RETURN category_id;
 
