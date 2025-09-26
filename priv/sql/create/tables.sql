d19e0d85d9c44e8d0ea0c2fe53f2e6f449be236d
Kacaii <kacaii.privado@gmail.com>
2025-09-26 18:34:14 -0300

    :boom: upgrade to postgresql@18
    uses uuidv7 instead of v4

--- priv/sql/create/tables.sql
+++ priv/sql/create/tables.sql
@@ -20,13 +20,13 @@ DROP TABLE IF EXISTS public.user_role;
 --   CREATE -----------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS public.user_role (
-    id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
+    id UUID PRIMARY KEY DEFAULT UUIDV7(),
     role_name TEXT NOT NULL,
     description TEXT
 );
 
 CREATE TABLE IF NOT EXISTS public.user_account (
-    id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
+    id UUID PRIMARY KEY DEFAULT UUIDV7(),
     role_id UUID REFERENCES public.user_role (id)
     ON UPDATE CASCADE ON DELETE SET NULL DEFAULT NULL,
     full_name TEXT NOT NULL,
@@ -46,14 +46,14 @@ CREATE INDEX IF NOT EXISTS idx_user_id
 ON public.user_account (id);
 
 CREATE TABLE IF NOT EXISTS public.brigade (
-    id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
+    id UUID PRIMARY KEY DEFAULT UUIDV7(),
     brigade_name TEXT DEFAULT NULL,
     description TEXT DEFAULT NULL,
     is_active BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE IF NOT EXISTS public.brigade_membership (
-    id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
+    id UUID PRIMARY KEY DEFAULT UUIDV7(),
     user_id UUID REFERENCES public.user_account (id)
     ON UPDATE CASCADE ON DELETE SET NULL,
     brigade_id UUID REFERENCES public.brigade (id)
@@ -67,7 +67,7 @@ CREATE INDEX IF NOT EXISTS idx_brigade_membership_brigade_id
 ON public.brigade_membership (brigade_id);
 
 CREATE TABLE IF NOT EXISTS public.occurrence_category (
-    id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
+    id UUID PRIMARY KEY DEFAULT UUIDV7(),
     parent_category_id UUID REFERENCES public.occurrence_category (id)
     ON UPDATE CASCADE ON DELETE CASCADE DEFAULT NULL,
     category_name TEXT UNIQUE NOT NULL,
@@ -76,7 +76,7 @@ CREATE TABLE IF NOT EXISTS public.occurrence_category (
 );
 
 CREATE TABLE IF NOT EXISTS public.occurrence (
-    id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
+    id UUID PRIMARY KEY DEFAULT UUIDV7(),
     applicant_id UUID REFERENCES public.user_account (id)
     ON UPDATE CASCADE ON DELETE SET NULL,
     category_id UUID REFERENCES public.occurrence_category (id)
