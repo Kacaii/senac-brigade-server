c3f0f89b1ff7bbb53ba7b5de14861f49df98730d
Kacaii <kacaii.privado@gmail.com>
2025-10-21 14:45:19 -0300

    :recycle: remove unused functions

--- priv/sql/create/functions.sql
+++ priv/sql/create/functions.sql
@@ -1,64 +1,44 @@
---   DROP -------------------------------------------------------------------
 BEGIN;
 
-DROP FUNCTION IF EXISTS public.query_user_id_by_registration;
-DROP FUNCTION IF EXISTS public.query_brigade_members_id;
-DROP FUNCTION IF EXISTS public.query_crew_members_id;
-DROP FUNCTION IF EXISTS public.query_all_occurrences_by_user_id;
+-- DROP ------------------------------------------------------------------------
+DROP FUNCTION IF EXISTS public.query_crew_members;
+DROP FUNCTION IF EXISTS public.query_occurrence_participants;
 
---   CREATE -------------------------------------------------------------------
-
---   Returns the user_account UUID by using their registration
-CREATE OR REPLACE FUNCTION public.query_user_id_by_registration(rg TEXT)
-RETURNS UUID AS $$
-
-DECLARE user_id UUID;
-
-BEGIN
-
-SELECT u.id INTO user_id
-  FROM public.user_account AS u
-WHERE u.registration = $1;
-
-RETURN user_id;
-
-END;
-$$ LANGUAGE plpgsql;
-
---   Returns all members of a brigade by using the brigade's UUID
-CREATE OR REPLACE FUNCTION public.query_brigade_members_id(brigade_id UUID)
-RETURNS SETOF UUID AS $$
-BEGIN
-
-RETURN QUERY
-
-SELECT
-    u.id
-FROM public.user_account AS u
-INNER JOIN public.brigade_membership AS bm ON u.id = bm.user_id
-WHERE bm.brigade_id = $1;
-
-END;
-$$ LANGUAGE plpgsql;
+-- CREATE ----------------------------------------------------------------------
 
 --   Returns all users that are in the same brigades as the target user
-CREATE OR REPLACE FUNCTION public.query_crew_members_id(user_id UUID)
-RETURNS SETOF UUID AS $$
+CREATE OR REPLACE FUNCTION public.query_crew_members(user_id UUID)
+RETURNS SETOF UUID
+LANGUAGE plpgsql
+AS $$
 BEGIN
-
-RETURN QUERY
-
-SELECT u.id
-FROM public.user_account AS u
-INNER JOIN public.brigade_membership AS bm
-    ON u.id = bm.user_id
-INNER JOIN public.brigade AS b
-    ON bm.brigade_id = b.id
-WHERE bm.brigade_id IN (
-    SELECT membership.brigade_id
-    FROM brigade_membership AS membership
-    WHERE membership.user_id = $1
-) AND u.id != $1;
-
+    RETURN QUERY
+    SELECT u.id
+    FROM public.user_account AS u
+    INNER JOIN public.brigade_membership AS bm ON u.id = bm.user_id
+    INNER JOIN public.brigade AS b ON bm.brigade_id = b.id
+    WHERE bm.brigade_id IN (
+        SELECT membership.brigade_id
+        FROM brigade_membership AS membership
+        WHERE membership.user_id = $1
+    ) AND u.id != $1;
 END;
-$$ LANGUAGE plpgsql;
+$$;
+
+--   Return all users that participated in a occurrence
+CREATE OR REPLACE FUNCTION public.query_occurrence_participants(occ_id UUID)
+RETURNS SETOF UUID
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT u.id
+    FROM public.user_account AS u
+    INNER JOIN public.brigade_membership AS bm ON bm.user_id = u.id
+    inner JOIN public.brigade AS b ON bm.brigade_id = b.id
+    inner JOIN public.occurrence AS o ON b.id = ANY(o.brigade_list)
+    where o.id = $1;
+END;
+$$;
+
+COMMIT;
