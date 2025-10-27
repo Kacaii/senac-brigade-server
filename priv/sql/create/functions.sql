3bb74af5ac9eba0cb2c51b5b3f52e5bb6dd504b2
Kacaii <kacaii.privado@gmail.com>
2025-10-27 08:54:25 -0300

    :fire: remove functions that use the array field

--- priv/sql/create/functions.sql
+++ priv/sql/create/functions.sql
@@ -2,42 +2,24 @@ BEGIN;
 
 -- DROP ------------------------------------------------------------------------
 DROP FUNCTION IF EXISTS public.query_crew_members;
-DROP FUNCTION IF EXISTS public.query_occurrence_participants;
 
 -- CREATE ----------------------------------------------------------------------
 
 --   Returns all users that are in the same brigades as the target user
-CREATE OR REPLACE FUNCTION public.query_crew_members(p_user_uuid UUID)
-RETURNS TABLE (member_uuid UUID, brigade_uuid UUID)
+CREATE OR REPLACE FUNCTION public.query_crew_members(p_user_id UUID)
+RETURNS TABLE (member_id UUID, brigade_id UUID)
 LANGUAGE plpgsql
 STABLE
 PARALLEL SAFE
 AS $$
 BEGIN
     RETURN QUERY
-    SELECT DISTINCT bm.user_id as member_uuid, bm.brigade_id as brigade_uuid
+    SELECT DISTINCT bm.user_id as member_id, bm.brigade_id as brigade_id
     FROM public.brigade_membership AS bm
     INNER JOIN public.brigade_membership AS target_bm
         ON bm.brigade_id = target_bm.brigade_id
-    WHERE target_bm.user_id = p_user_uuid
-        AND bm.user_id <> p_user_uuid;
-END;
-$$;
-
---   Return all users that participated in a occurrence
-CREATE OR REPLACE FUNCTION public.query_occurrence_participants(p_occ_id UUID)
-RETURNS TABLE (id UUID)
-LANGUAGE plpgsql
-STABLE
-PARALLEL SAFE
-AS $$
-BEGIN
-    RETURN QUERY
-    SELECT DISTINCT u.id
-    FROM public.occurrence AS occ
-    INNER JOIN public.brigade AS b ON b.id = ANY(occ.brigade_list)
-    INNER JOIN public.user_account as u on u.id = ANY(b.members_id)
-    WHERE occ.id = p_occ_id;
+    WHERE target_bm.user_id = p_user_id
+        AND bm.user_id <> p_user_id;
 END;
 $$;
 
